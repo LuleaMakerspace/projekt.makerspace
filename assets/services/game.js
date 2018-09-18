6087cfb125026c27347098791b6d109ff95bb860
Victor Forsgren <victor@forsgrens.nu>
2018-09-18 20:32:44 +0200

    Hämtar från projekt.json istället

--- assets/services/game.js
+++ assets/services/game.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
     async getGames() {
         try {
-            let spel = await axios.get('/spel.json')
+            let spel = await axios.get('https://raw.githubusercontent.com/LuleaMakerspace/projekt-lista/master/projekt.json')
             return spel.data
         } catch (e) {
             return []
