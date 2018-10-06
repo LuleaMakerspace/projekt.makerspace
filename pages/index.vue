e235fe711bff7344b215a43f12b4f334bd4804d0
Victor Forsgren <forsgren.victor@gmail.com>
2018-10-06 11:27:48 +0200

    test3

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,6 @@
   <div class="container">
     <full-view :big="'Utvalda'" :small="'Uppdateras dagligen'" :projects="projects.slice(0, 3)"></full-view>
     <full-view :big="'Utforska'" :projects="projects.slice(3)"></full-view>
-    <b-form inline name="contact" method="POST" netlify>
-      <label class="sr-only" for="inlineFormInputName2">Name</label>
-      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="Jane Doe" />
-      <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-      <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
-        <b-input id="inlineFormInputGroupUsername2" placeholder="Username" />
-      </b-input-group>
-      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
-      <b-button variant="primary">Save</b-button>
-    </b-form>
   </div>
 </template>
 
