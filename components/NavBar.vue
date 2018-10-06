c582a4df575bcd8118ccbe7b2ad6b6da26e3d59c
Victor Forsgren <forsgren.victor@gmail.com>
2018-10-06 11:58:41 +0200

    Länk till google form

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,7 +8,7 @@
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
         <b-collapse is-nav id="nav_collapse">
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="https://github.com/LuleaMakerspace/projekt-lista">Ladda upp</b-nav-item>
+                    <b-nav-item href="https://goo.gl/forms/q8WVHKWDTmqfwRtJ2">Ladda upp</b-nav-item>
                     <b-nav-form @submit="onSearch">
                         <b-input-group>
                             <b-form-input size="sm" class="mr-sm-2" type="text" v-model="query" required placeholder="Vad letar du efter?"/>
