5272f7ca2ceaa071491ae5a97c184c10339d0613
Victor Forsgren <forsgren.victor@gmail.com>
2020-01-16 17:42:46 +0100

    improved play icons

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -4,16 +4,22 @@
       <b-row>
         <b-col cols="6" md="5" style="height: 100px;">
           <img :src="project.image" style="width: 100%; height: 100%; object-fit: cover;" />
-          <span style="position: absolute; color: #9afe7a; bottom: 10px; right: 30px; text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.65);">
-              <span v-if="project.download"><i class="fas fa-file-archive"></i></span>
-              <span v-if="project.embed"><i class="fas fa-play"></i></span>
-              <span v-if="project.extern"><i class="fas fa-external-link-square-alt"></i></span>
+          <span class="play-icons">
+            <span v-if="project.download">
+              <i class="fas fa-file-archive"></i>
             </span>
+            <span v-if="project.embed">
+              <i class="fas fa-play"></i>
+            </span>
+            <span v-if="project.extern">
+              <i class="fas fa-external-link-square-alt"></i>
+            </span>
+          </span>
         </b-col>
-        <b-col style="font-family: brutal;">
-            <span>{{project.title}}</span>
+        <b-col class="title-authors">
+          <span>{{project.title}}</span>
           <br />
-          <span style="color: rgba(0, 0, 0, 0.7);">
+          <span class="authors">
             <authors :list="project.authors"></authors>
           </span>
         </b-col>
@@ -33,14 +39,37 @@ export default {
 };
 </script>
 
-<style>
-.projectcard a {
-  color: inherit; /* blue colors for links too */
-  text-decoration: inherit;
-}
+<style lang="scss">
+.projectcard {
+  .play-icons {
+    position: absolute;
+    color: rgba(62, 255, 0, 0.68);
+    bottom: 10px;
+    right: 30px;
+    text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.65);
+    transition: 0.2s ease-in-out;
+    transition-property: color, transform;
+    transform-origin: 20 20;
+  }
+  a {
+    color: inherit; /* blue colors for links too */
+    text-decoration: inherit;
+  }
 
-.projectcard a:hover {
-  color: inherit; /* blue colors for links too */
-  text-decoration: inherit;
+  a:hover {
+    color: inherit; /* blue colors for links too */
+    text-decoration: inherit;
+  }
+  .title-authors {
+    font-family: brutal;
+    .authors {
+      color: rgba(0, 0, 0, 0.7);
+    }
+  }
+}
+.projectcard:hover {
+  .play-icons {
+    color: rgba(62, 255, 0, 1);
+  }
 }
 </style>
