6e1653f184226f783a4fe2dfa7ed388016bc95a1
Victor Forsgren <forsgren.victor@gmail.com>
2020-02-04 20:08:38 +0100

    huvudlinjer

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <div class="container" style="padding-top: 40px; padding-bottom: 40px;">
-      <h1 style="font-size: 3.5em;">Spel, Luleå Makerspace</h1>
+      <b-row>
+        <b-col cols="12" lg="6" style="display: flex;
+  justify-content: center;
+  flex-direction: column;">
+          <h1 style="font-size: 3.5em;" class="section">Spel, Luleå Makerspace</h1>
+        </b-col>
+        <b-col cols="12" lg="6" class="section-highlight">
+          <project-card :project="recommended[0]"></project-card>
+          <div>
+            <i class="fas fa-quote-left"></i>
+            <span v-if="recommended[0].comment">{{recommended[0].comment}}</span>
+            <span v-else>{{recommended[0].description}}</span>
+            <i class="fas fa-quote-right"></i>
+          </div>
+        </b-col>
+      </b-row>
     </div>
     <div class="container">
       <div class="section section-highlight" v-if="recommended">
         <p>
-          <b>Rekommenderade spel ({{recommended.length}})</b>
+          <b>Rekommenderade spel ({{recommended.length - 1}})</b>
         </p>
-        <project-list-horizontal :projects="recommended"></project-list-horizontal>
+        <project-list-horizontal :projects="recommended.slice(1)"></project-list-horizontal>
       </div>
       <div class="section section-highlight" v-if="newArrivals">
         <p>
@@ -29,6 +44,7 @@
 <script>
 import ProjectListHorizontal from "~/components/ProjectListHorizontal.vue";
 import ProjectList from "~/components/ProjectList.vue";
+import ProjectCard from "~/components/ProjectCard.vue";
 
 import { projectCollection } from "~/plugins/firebase.js";
 import { getProjects } from "~/services/projectService.js";
@@ -38,7 +54,8 @@ import { mapState } from "vuex";
 export default {
   components: {
     ProjectListHorizontal,
-    ProjectList
+    ProjectList,
+    ProjectCard
   },
   async asyncData({ error }) {
     try {
@@ -46,7 +63,10 @@ export default {
         projectCollection.orderBy("title", "asc")
       );
       let recommended = await getProjects(
-        projectCollection.where("recommended", "==", true)
+        projectCollection
+          .where("recommended", "==", true)
+          .orderBy("published", "desc")
+          .limit(6)
       );
       let newArrivals = await getProjects(
         projectCollection.orderBy("published", "desc").limit(6)
